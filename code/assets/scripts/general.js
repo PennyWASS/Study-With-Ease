103eee5bff56c35879bcf1f684a6f8118cb75d5a
Felipe <102631323+okamitk@users.noreply.github.com>
2022-10-09 20:10:43 -0300

    Add files via upload

--- code/assets/scripts/general.js
+++ code/assets/scripts/general.js
@@ -11,3 +11,28 @@ function toggledarkmode() {
         document.getElementById("darkmode").innerHTML = ('<i class="fa-solid fa-sun"></i>')
     }
 }
+
+const obj = document.querySelector(".popup");
+obj.addEventListener("mousedown", mousedown);
+
+function mousedown(e) {
+    window.addEventListener("mousemove", mousemove);
+    window.addEventListener("mouseup", mouseup);
+    let prevX = e.clientX;
+    let prevY = e.clientY;
+
+    function mousemove(e) {
+        let newX = prevX - e.clientX;
+        let newY = prevY - e.clientY;
+        const rect = obj.getBoundingClientRect();
+        obj.style.left = rect.left - newX + "px";
+        obj.style.top = rect.top - newY + "px";
+        prevX = e.clientX;
+        prevY = e.clientY;
+    }
+
+    function mouseup() {
+        window.removeEventListener('mousemove', mousemove);
+        window.removeEventListener('mouseup', mouseup);
+    }
+}
