00695dec875a797784ab9c34d5a88b17312e6ee2
Felipe <102631323+okamitk@users.noreply.github.com>
2022-10-09 23:26:59 -0300

    Add files via upload

--- code/assets/scripts/general.js
+++ code/assets/scripts/general.js
@@ -12,27 +12,72 @@ function toggledarkmode() {
     }
 }
 
-const obj = document.querySelector(".popup");
-obj.addEventListener("mousedown", mousedown);
+function movepopup(popup) {
+    let obj = document.querySelector("." + popup);
+    obj.addEventListener("mousedown", mousedown);
 
-function mousedown(e) {
-    window.addEventListener("mousemove", mousemove);
-    window.addEventListener("mouseup", mouseup);
-    let prevX = e.clientX;
-    let prevY = e.clientY;
+    function mousedown(e) {
+        window.addEventListener("mousemove", mousemove);
+        window.addEventListener("mouseup", mouseup);
+        let prevX = e.clientX;
+        let prevY = e.clientY;
 
-    function mousemove(e) {
-        let newX = prevX - e.clientX;
-        let newY = prevY - e.clientY;
-        const rect = obj.getBoundingClientRect();
-        obj.style.left = rect.left - newX + "px";
-        obj.style.top = rect.top - newY + "px";
-        prevX = e.clientX;
-        prevY = e.clientY;
+        function mousemove(e) {
+            let newX = prevX - e.clientX;
+            let newY = prevY - e.clientY;
+            const rect = obj.getBoundingClientRect();
+            obj.style.left = rect.left - newX + "px";
+            obj.style.top = rect.top - newY + "px";
+            prevX = e.clientX;
+            prevY = e.clientY;
+        }
+
+        function mouseup() {
+            window.removeEventListener('mousemove', mousemove);
+            window.removeEventListener('mouseup', mouseup);
+        }
     }
+}
+movepopup("popup")
 
-    function mouseup() {
-        window.removeEventListener('mousemove', mousemove);
-        window.removeEventListener('mouseup', mouseup);
+let popupcounter = 1
+
+function createpin() {
+    popupcounter = popupcounter + 1;
+    const pin = document.createElement("div");
+    pin.classList.add("popup", "popup" + popupcounter);
+    document.getElementById("popups").appendChild(pin);
+    let randomkaomoji = (["(×﹏×)", "(￢_￢;)", "(✿◠‿◠)", "(⁄ ⁄•⁄ω⁄•⁄ ⁄)", "ᕕ( ᐛ )ᕗ", "(¬ ¬ )", "( Φ ω Φ )", "U・ᴥ・U", "／(≧ x ≦)＼", "／(˃ᆺ˂)＼", "( ˘ ɜ˘) ♬♪♫", "(^_^♪)", "(ˊ〇ˋ*)", "(`･Θ･´)", "／(^ × ^)＼"])[Math.floor(Math.random() * 14)];
+    document.querySelector(".popup" + popupcounter).innerHTML = ('<div class="popheader"><button class="close" onclick="remove(this)"><i class="fa-solid fa-x"></i></button><div class="poptitle"><span class="input" role="textbox" contenteditable>' + "post-it " + popupcounter + "!" + '</span></div><button class="minimize" onclick="minimize(this)"><i class="fa-solid fa-window-minimize"></i></button></div><div class="poptextarea"><span class="input" role="textbox" contenteditable>' + randomkaomoji + '</span></div>');
+    movepopup("popup" + popupcounter)
+}
+
+function remove(el) {
+    var child = el
+    var parent = child.parentElement;
+    var grandparent = parent.parentElement;
+    grandparent.remove();
+}
+let iconcounter = 0;
+
+function minimize(el) {
+    var child = el;
+    var parent = child.parentElement;
+    var grandparent = parent.parentElement;
+    grandparent.classList.add("minimized")
+    const icon = document.createElement("button")
+    icon.classList.add("icon", "icon" + iconcounter)
+    document.getElementById("toolbar").appendChild(icon)
+    document.querySelector(".icon" + iconcounter).innerHTML = ('<div class="' + iconcounter + '" onclick="deminimize(this)"><i class="fa-solid fa-thumbtack"></i>' + (iconcounter + 1) + '</div>')
+    iconcounter = iconcounter + 1;
+}
+
+function deminimize(el) {
+    if (el.className == 0) {
+        document.querySelector(".popup").classList.remove("minimized");
+        el.parentElement.remove();
+    } else {
+        document.querySelector(".popup" + (el.className + 1)).classList.remove("minimized");
+        el.parentElement.remove();
     }
 }
